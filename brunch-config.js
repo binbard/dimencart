9f58f23e17f75ee6eb9e87cd333b91236aaa0cd5
uti1man <harshitjawla123@gmail.com>
2022-02-13 01:03:05 +0530

    little config

---
+++ brunch-config.js
@@ -0,0 +1,5 @@
+exports.files = {
+  javascripts: {joinTo: 'app.js'},
+  stylesheets: {joinTo: 'app.css'},
+  templates: {joinTo: 'app.js'}
+};
---
+++ purgecss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['index.html','aboutus.html'],
+  css: [
+  './node_modules/bootstrap/dist/css/bootstrap.min.css',
+  './src/source/Semantic-UI-CSS-master/semantic.min.css',
+  './node_modules/swiper/swiper-bundle.min.css',
+  './node_modules/animate.css/animate.min.css',
+  './node_modules/@mdi/font/css/materialdesignicons.min.css',
+  './src/css/*.css',
+  ]
+}
