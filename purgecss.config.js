92e52e9db1f0f3ecb7b7032c4ebad06c9976e97d
uti1man <harshitjawla123@gmail.com>
2022-02-13 01:35:00 +0530

    small comit

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  output: "dist/css",
   content: ['index.html','aboutus.html'],
   css: [
   './node_modules/bootstrap/dist/css/bootstrap.min.css',
-  './src/source/Semantic-UI-CSS-master/semantic.min.css',
+  './node_modules/fomantic-ui-css/semantic.min.css',
   './node_modules/swiper/swiper-bundle.min.css',
   './node_modules/animate.css/animate.min.css',
   './node_modules/@mdi/font/css/materialdesignicons.min.css',
