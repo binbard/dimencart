a1f0628cd78c4f1423779da3ebb0a5f00db8468d
Amit-TheOne <amit.dew009@gmail.com>
2022-10-04 14:08:08 +0530

    added responsiveness to navigation

--- src/js/script.js
+++ src/js/script.js
@@ -103,3 +103,10 @@ function logout() {
 
 $('.ui.button.toggle').state()
 
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
